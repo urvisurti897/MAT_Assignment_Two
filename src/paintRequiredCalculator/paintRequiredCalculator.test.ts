aa0ce77fe011d0336505a936b86a0452d09584b1
Urvi <urvisurti897@gmail.com>
2025-03-12 15:36:25 -0400

    paintRequiredCalculator.test.ts: Adding test to validate zero

--- src/paintRequiredCalculator/paintRequiredCalculator.test.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.test.ts
@@ -3,14 +3,20 @@ import paintRequiredCalculator from "./paintRequiredCalculator";
 describe("paint required calculator", () => {
   test("should return expected result", () => {
     const result = paintRequiredCalculator(50, 10);
-
     expect(result).toEqual(5);
   });
 
-  test.todo("should throw an error when area is zero");
-  test.todo("should throw an error when coveragePerLiter is zero");
+  test("should throw an error when area is zero", () => {
+    expect(() => paintRequiredCalculator(0, 10)).toThrow("Area should not be zero");
+  });
+
+  test("should throw an error when coveragePerLiter is zero", () => {
+    expect(() => paintRequiredCalculator(50, 0)).toThrow("Coverage Per Liter should not be zero");
+  });
+
   test.todo("should throw an error when area is negative");
   test.todo("should throw an error when coveragePerLiter is negative");
-  test.todo("should handle when area is decimal number");
-  test.todo("should handle when coveragePerLiter is decimal number");
+
+  test.todo("should handle when area is a decimal number");
+  test.todo("should handle when coveragePerLiter is a decimal number");
 });
--- src/paintRequiredCalculator/paintRequiredCalculator.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.ts
@@ -1,3 +1,20 @@
 export default function paintRequiredCalculator(area: number, coveragePerLiter: number): number {
+
+  if (area == 0){
+    throw new Error("Area should not be zero");
+  }
+
+  if (coveragePerLiter == 0){
+    throw new Error("Coverage Per Liter should not be zero");
+  }
+
+  if (area < 0){
+    throw new Error("Area should be positive");
+  }
+
+  if (coveragePerLiter < 0){
+    throw new Error("Coverage Per Liter should be positive");
+  }
+
   return area / coveragePerLiter;
 }
