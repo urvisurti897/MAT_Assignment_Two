1afa29392c4c5f802ad5e0c1055cce5a2c85e847
Urvi <urvisurti897@gmail.com>
2025-03-12 15:17:45 -0400

    calculateCanvasSize.test.ts: Adding test for validating negative number

--- src/calculateCanvasSize/calculateCanvasSize.test.ts
+++ src/calculateCanvasSize/calculateCanvasSize.test.ts
@@ -3,12 +3,18 @@ import calculateCanvasSize from "./calculateCanvasSize";
 describe("calculateCanvasSize", () => {
   test("returns correct result", () => {
     const result = calculateCanvasSize("10", "100");
-
     expect(result).toEqual(1000);
-  })
+  });
 
-  test.todo("should throw an error if length is negative");
-  test.todo("should throw an error if width is negative");
+  test("should throw an error if length is negative", () => {
+    expect(() => calculateCanvasSize("-10", "100")).toThrow("Length should be positive");
+  });
+
+  test("should throw an error if width is negative", () => {
+    expect(() => calculateCanvasSize("10", "-100")).toThrow("Width should be positive");
+  });
+
+
   test.todo("should throw an error if length is zero");
   test.todo("should throw an error if width is zero");
   test.todo("should throw an error if length is non-numeric");
--- src/calculateCanvasSize/calculateCanvasSize.ts
+++ src/calculateCanvasSize/calculateCanvasSize.ts
@@ -2,5 +2,21 @@ export default function calculateCanvasSize(
   length: string,
   width: string
 ) {
-  return parseInt(length) * parseInt(width);
+
+  const lengthNum = parseInt(length);
+  const widthNum = parseInt(width);
+
+  if (isNaN(lengthNum) || isNaN(widthNum)) {
+    throw new Error("Both length and width must be valid numbers");
+  }
+
+  if (lengthNum < 0){
+    throw new Error("Length should be positive");
+  }
+
+  if (widthNum < 0){
+    throw new Error("Width should be positive");
+  }
+
+  return (lengthNum) * (widthNum);
 }
